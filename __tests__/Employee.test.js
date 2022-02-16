75684c5cced079b4d0eaf674645b01e188a97d44
Expired <imoses2@hotmail.com>
2022-02-15 22:28:26 -0700

    more tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,6 +4,25 @@ test('creates an employee object', () => {
 	const employee = new Employee('Alice', 1, 'Alice@company.com');
 	
 	expect(employee.name).toBe('Alice');
-	expect(employee.id).toBe(0);
+	expect(employee.id).toBe(1);
 	expect(employee.email).toBe('Alice@company.com');
 });
+
+test('gets player name', () => {
+	const employee = new Employee('Alice', 1, 'Alice@company.com');
+	
+	expect(employee.getName()).toBe('Alice');
+});
+
+test('gets player id', () => {
+	const employee = new Employee('Alice', 1, 'Alice@company.com');
+	
+	expect(employee.getId()).toBe(1);
+});
+
+test('gets player email', () => {
+	const employee = new Employee('Alice', 1, 'Alice@company.com');
+	
+	expect(employee.getEmail()).toBe('Alice@company.com');
+});
+
