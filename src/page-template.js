8cd299125ec015dbae56b90a94c63dbb6022bf6e
Expired <imoses2@hotmail.com>
2022-02-16 00:56:34 -0700

    mvp done

--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,33 @@
-const pageTemplate = `
-  <!DOCTYPE html>
+const buildExtra = employee => {
+	const role = employee.getRole();
+	if (role == 'Manager')
+		return `Office Number: ${employee.getOfficeNumber()}`;
+	if (role == 'Engineer')
+		return `GitHub: ${employee.getGithub()}`;
+	if (role == 'Intern')
+		return `School: ${employee.getSchool()}`;
+};
+
+const cardTemplate = employee => 
+`			<div class='card'>
+				<h2>${employee.getName()}</h2>
+				<h3>${employee.getRole()}</h3>
+				<p>ID: ${employee.getId()}</p>
+				<p>Email: ${employee.getEmail()}</p>
+				<p>${buildExtra(employee)}</p>
+			</div>
+`;
+
+const buildMain = employeeList => {
+	let result = ``;
+	for(let i=0; i<employeeList.length; i++){
+		result = result.concat(cardTemplate(employeeList[i]));
+	}
+	return result;
+}
+
+const pageTemplate = employeeList => 
+`  <!DOCTYPE html>
   <html lang="en">
 
   <head>
@@ -15,15 +43,12 @@ const pageTemplate = `
 			<h1>Team Portfolio</h1>
     </header>
     <main>
-    </main>
+${buildMain(employeeList)}    </main>
     <footer>
     </footer>
   </body>
-  </html>
-  `;
+  </html>`;
 
-const cardTemplate = ``;
-
-module.exports = templateData => {
-	return pageTemplate;
+module.exports = employeeList => {
+	return pageTemplate(employeeList);
 }
