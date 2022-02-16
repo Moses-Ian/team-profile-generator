41fbc8c202ab30f8cad128a89011c1548b9561a7
Expired <imoses2@hotmail.com>
2022-02-15 22:38:17 -0700

    update and pass first test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,11 @@
 const Manager = require('../lib/Manager.js');
 
 test('creates a manager object', () => {
-	const manager = new Manager('Alice', 1, 'Alice@company.com');
+	const manager = new Manager('Alice', 1, 'Alice@company.com', 5);
 	
 	expect(manager.name).toBe('Alice');
 	expect(manager.id).toBe(1);
 	expect(manager.email).toBe('Alice@company.com');
+	expect(manager.officeNumber).toBe(5);
 });
 
